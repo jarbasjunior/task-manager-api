18ed06975694bb00db66a0952f47b3a5b993e60e
Jarbas Junior <jarbasoujunior@gmail.com>
2019-10-29 19:23:18 -0300

    Addin create action to the users controller with tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,28 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
+
+  def create
+    user = User.new(user_params)
+    # render json: user, status: 201 if user.save
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
   def show
     begin
-      @user = User.find(params[:id])
-      respond_with @user
+      user = User.find(params[:id])
+      respond_with user
     rescue
       head 404
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
