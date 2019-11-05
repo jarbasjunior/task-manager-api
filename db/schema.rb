403637db562e55d43d0b9e7cfc35eebaab2b9708
Jarbas Junior <jarbasoujunior@gmail.com>
2019-11-05 17:15:56 -0300

    Adding auth token and #info to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_191_024_174_838) do
+ActiveRecord::Schema.define(version: 20_191_031_170_925) do
   create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20_191_024_174_838) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'auth_token'
+    t.index ['auth_token'], name: 'index_users_on_auth_token', unique: true
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
