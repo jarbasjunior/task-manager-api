5c02d7179d80c50cffb693ab19a7f939c331553f
Jarbas Junior <jarbasoujunior@gmail.com>
2019-12-01 23:08:43 -0300

    Adding association with tasks for user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
 
   validates_uniqueness_of :auth_token
   before_create :generate_authentication_token!
+
+  has_many :tasks, dependent: :destroy
 
   def info
     "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
