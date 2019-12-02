2064b7410d189836483006b1f27958090ce4c12e
Jarbas Junior <jarbasoujunior@gmail.com>
2019-12-01 22:28:14 -0300

    Adding task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_170925) do
+ActiveRecord::Schema.define(version: 20_191_202_011_622) do
+  create_table 'tasks', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.string 'title'
+    t.text 'description'
+    t.boolean 'done', default: false
+    t.datetime 'deadline'
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_tasks_on_user_id'
+  end
+
   create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -24,4 +35,6 @@ ActiveRecord::Schema.define(version: 2019_10_31_170925) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key 'tasks', 'users'
 end
