588add262bf091377556b727252a3f51cbdb54ea
Jarbas Junior <jarbasoujunior@gmail.com>
2019-12-01 21:49:49 -0300

    Adding authorization for the users controller

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,6 +4,6 @@ module Authenticable
   end
 
   def authenticate_with_token!
-    render json: { errors: 'Unauthorized access!' }, status: 401 #unless current_user.present?
+    render json: { errors: 'Unauthorized access!' }, status: 401 unless current_user.present?
   end
 end
