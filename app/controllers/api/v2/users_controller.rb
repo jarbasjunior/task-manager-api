37b62b937b7883bb842694c03c60b8c719617232
Jarbas Junior <jarbasoujunior@gmail.com>
2019-12-03 14:47:50 -0300

    Adding API::V2

---
+++ app/controllers/api/v2/users_controller.rb
@@ -0,0 +1,45 @@
+class Api::V2::UsersController < ApplicationController
+  before_action :authenticate_with_token!, only: %i[update destroy]
+
+  respond_to :json
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def index
+    render json: { users: User.all }, status: 200
+  end
+
+  def show
+    user = User.find(params[:id])
+    respond_with user
+  rescue StandardError
+    head 404
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    head 204
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
